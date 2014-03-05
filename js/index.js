1b59a668f66f002319be5cc9d12bd2f7916cca84
Jelle bij Nettt <jelle@nettt.nl>
2014-03-05 09:55:45 +0100

    fix

--- js/index.js
+++ js/index.js
@@ -38,31 +38,6 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
 		
-		listenToSensor();
-		
-		while (start == true)
-		{
-			if(acceleration.y > 7){
-				$('body').css('background', 'red !important');
-			}else {
-				$('body').css('background', 'none !important');
-			}
-		}
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -77,54 +52,4 @@ var app = {
     }
 };
 
-function listenToSensor() {
-		
-	// The watch id references the current `watchAcceleration`
-	var watchID = null;
 
-	// Wait for device API libraries to load
-	//
-	document.addEventListener("deviceready", onDeviceReady, false);
-
-	// device APIs are available
-	//
-	function onDeviceReady() {
-		startWatch();
-	}
-
-	// Start watching the acceleration
-	//
-	function startWatch() {
-
-		// Update acceleration every 3 seconds
-		var options = { frequency: 300 };
-
-		watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
-	}
-
-	// Stop watching the acceleration
-	//
-	function stopWatch() {
-		if (watchID) {
-			navigator.accelerometer.clearWatch(watchID);
-			watchID = null;
-		}
-	}
-
-	// onSuccess: Get a snapshot of the current acceleration
-	//
-	function onSuccess(acceleration) {
-		var element = document.getElementById('accelerometer');
-		element.innerHTML = 'Acceleration X: ' + acceleration.x         + '<br />' +
-							'Acceleration Y: ' + acceleration.y         + '<br />' +
-							'Acceleration Z: ' + acceleration.z         + '<br />' +
-							'Timestamp: '      + acceleration.timestamp + '<br />';
-		start = true;
-	}
-
-	// onError: Failed to get the acceleration
-	//
-	function onError() {
-		alert('onError!');
-	}
-}
